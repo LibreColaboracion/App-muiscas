ceaed41f604cdbfb3a9a680b48de027173e79368
@carlosaalf <carlosalfonso144@gmail.com>
2016-02-22 14:10:41 -0500

    Tips para HTML y CSS mas cambios generales en app

--- app-muiscas/platforms/android/platform_www/cordova_plugins.js
+++ app-muiscas/platforms/android/platform_www/cordova_plugins.js
@@ -5,12 +5,21 @@ module.exports = [
         "id": "cordova-plugin-whitelist.whitelist",
         "pluginId": "cordova-plugin-whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.1"
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-device": "1.1.1"
 }
 // BOTTOM OF METADATA
 });
