bad618a8075e124064b9d5da7fe97b56cf5f5e97
@carlosaalf <carlosalfonso144@gmail.com>
2016-03-08 21:35:46 -0500

    algunos cacharreos de android y chat

--- app-muiscas/platforms/android/platform_www/cordova_plugins.js
+++ app-muiscas/platforms/android/platform_www/cordova_plugins.js
@@ -1,25 +1,23 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
-        "runs": true
-    },
-    {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
-        "pluginId": "cordova-plugin-device",
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-device": "1.1.1"
-}
+    "cordova-plugin-device": "1.1.1",
+    "cordova-plugin-whitelist": "1.2.1"
+};
 // BOTTOM OF METADATA
 });
