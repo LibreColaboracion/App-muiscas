360c6e5f801993597e07f7aff177e725f60d7893
MoiRouhs <carsrouhs@gmail.com>
2016-03-22 00:48:29 -0500

    terminado ejercicios 1

--- app-muiscas/www/saludos/js/saludos.js
+++ app-muiscas/www/saludos/js/saludos.js
@@ -1,3 +1,42 @@
+function comenzar(id1,clase,id2){
+    var input = document.getElementById("sub");
+    var ejer1 = document.getElementById(id1);
+    var borrar = document.getElementById(id2);
+    ejer1.className = clase;
+    borrar.style.display = "none";
+    input.style.display ="flex";
+}
+// Para los ejercicios
+var contar = 0;
+function escribir(id,respuesta,event) {
+    var enter = event.keyCode;
+    var input = document.getElementById("sub");
+    var val = document.getElementById(id).value;
+    var cuerpo=document.getElementById("secundario");
+    var valor = val.toLowerCase(); 
+    console.log(valor,respuesta, enter)
+    
+    if(enter == 13 | enter == undefined){
+        console.log("oui")
+	if(valor == respuesta){
+            cuerpo.innerHTML +='<div class="contderecha"><div class="burbuja derecha"><div class="contexto"><p class="texto">'+respuesta+'</p></div></div><img class="caras" src="imagenes/chatmujer.svg"></div>'
+        }else{
+            contar++;
+            switch(contar){
+                case 1:
+                    cuerpo.innerHTML +='<div class="contizquierda"><img class="caras" src="imagenes/chathombre.svg"><div class="burbuja izquierda"><div class="contexto"><p class="texto">Tu respuesta esta mal</p></div></div>';
+                break;
+                case 2:
+                    cuerpo.innerHTML +='<div class="contizquierda"><img class="caras" src="imagenes/chathombre.svg"><div class="burbuja izquierda"><div class="contexto"><p class="texto">Tu respuesta esta mal</p></div></div>';
+                break;
+                case 3:
+                    cuerpo.innerHTML +='<div class="contizquierda"><img class="caras" src="imagenes/chathombre.svg"><div class="burbuja izquierda"><div class="contexto"><p class="texto">vulve a estudiar</p></div></div>';
+                    put.style.display ="none";
+                break;
+            }
+        };
+    }
+}
 //  Esta funcion muestra el saludo su explicacion
 function hola(saludo, explicacion,audio, zoom1, zoom2, zoom3, zoom4){
     var titulo = document.getElementById("e1");
