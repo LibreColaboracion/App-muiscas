23086057004e6a08f5cb8b58380be85a77d231be
carlosalfonso144 <carlosalfonso144@gmail.com>
2016-03-28 21:25:44 -0500

    borrando carpetas de compilacion android

--- app-muiscas/platforms/android/assets/www/rondadecolores/js/rondadecolores.js
+++
@@ -1,8 +0,0 @@
-function efectos(id,clase){
-        var a = document.getElementById(id);
-        a.className = (clase);
-}
-function escuchar(id){
-        var a = document.getElementById(id);
-        a.play();
-    } 
