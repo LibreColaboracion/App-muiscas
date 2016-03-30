e49952d15afd9c1e60786611ade8d3ebd4fbd167
Nanamu <luzmurcia2@gmail.com>
2016-03-30 03:15:00 -0500

    arregos pagina principal ronda decolores, estrofa 1

--- app-muiscas/www/rondadecolores/js/rondadecolores.js
+++ app-muiscas/www/rondadecolores/js/rondadecolores.js
@@ -12,4 +12,36 @@ function efectos(id,clase){
 function escuchar(id){
         var a = document.getElementById(id);
         a.play();
-    } 
+    }
+ contador=0;
+    function tiempo(){
+        var c = setInterval(arcoiris,1000);
+    }
+    function arcoiris(){
+        contador++;
+        var texto = document.getElementById("lebique1");
+            switch(contador){
+                    case 1:
+                        texto.style.color="#ff5722";
+                    break;
+                    case 2:
+                        texto.style.color="#ff9100";
+                    break;
+                    case 3:
+                        texto.style.color="#ffea00";
+                    break;
+                    case 4:
+                        texto.style.color="#76ff03";
+                    break;
+                    case 5:
+                        texto.style.color="#4fc3f7";
+                    break;
+                    case 6:
+                        texto.style.color="#7e57c2";
+                    break;
+                    case 7:
+                        texto.style.color="#ec407a";
+                        contador=0;
+                    break;
+                } 
+         }
